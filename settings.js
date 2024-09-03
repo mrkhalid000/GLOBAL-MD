ec501324597e7deebda334a6dbe76c3d450fbf7a
mrkhalid000 <kingcoding11@gmail.com>
2024-09-03 19:57:55 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,19 +9,19 @@ global.socialm = "GitHub: GlobalTechInfo" //ur github or insta name
 global.location = "Pakistan, Karachi, Karachi" //ur location
 
 //new
-global.botname = '© 𝐆𝐋𝐎𝐁𝐀𝐋-𝐌𝐃' //ur bot name
-global.ownernumber = ['923444844060'] //ur owner number, dont add more than one
-global.ownername = '© GlobalTechInfo' //ur owner name
+global.botname = 'ᏦᏆᑎᏀ ᏦᕼᗩしᏆᗞ MD' //ur bot name
+global.ownernumber = ['2348023898600'] //ur owner number, dont add more than one
+global.ownername = 'ᏦᏆᑎᏀ ᏦᕼᗩしᏆᗞ' //ur owner name
 global.websitex = "https://www.youtube.com/@GlobalTechInfo"
 global.wagc = "https://whatsapp.com/channel/0029VagJIAr3bbVBCpEkAM07"
 global.themeemoji = '🪀'
 global.wm = "Xeon Bot Inc."
 global.botscript = 'https://github.com/GlobalTechInfo/GLOBAL-MD' //script link
 global.packname = "Sticker By"
-global.author = "GlobalTechInfo\n\n+923444844060"
-global.creator = "923444844060@s.whatsapp.net"
+global.author = "GlobalTechInfo\n\n+2348023898600"
+global.creator = "2348023898600@s.whatsapp.net"
 global.xprefix = '.'
-global.premium = ["923444844060"] // Premium User
+global.premium = ["2348023898600"] // Premium User
 
 //channel id
 global.xchannel = {
